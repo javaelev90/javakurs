b111beea93ee706507b64373d432b6ac70ee4581
javaelev90 <37899614+javaelev90@users.noreply.github.com>
2018-04-09 10:58:49 +0200

    Fixed bug. Should count words correctly now.

--- java1/src/java1/WordCounter.java
+++ java1/src/java1/WordCounter.java
@@ -1,18 +1,11 @@
 package java1;
 
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class WordCounter implements Counter {
 
 	@Override
 	public int count(String text) {
-		Pattern pattern = Pattern.compile("\\w");
-		// Splits on word boundaries, then filter out non-words and collects them into a list
-		List<String> words = Stream.of(text.split("\\b")).filter(pattern.asPredicate()).collect(Collectors.toList());
-		return words.size();
+		String[] words = text.split("[\\s]");
+		return words.length;
 	}
 
 }
