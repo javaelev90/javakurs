90e8fa5b296e1ca07f3191a7524b4b0199665357
javaelev90 <37899614+javaelev90@users.noreply.github.com>
2018-04-16 19:12:11 +0200

    Started making a transition but don't think i will continue

--- java1/src/java1/Main.java
+++ java1/src/java1/Main.java
@@ -8,70 +8,78 @@ public class Main {
 	public static void main(String[] args) {
 		boolean wantToExit = false;
 
-		try (InputReader reader = new InputReader(new InputStreamReader(System.in))){
-			while(!wantToExit) {
+		try (InputReader reader = new InputReader(new InputStreamReader(System.in))) {
+			while (!wantToExit) {
 				OutputWriter.printMainMenu();
 				char choice = reader.getInputChoice();
+				String text;
 				try {
-					
 
-					switch(choice) {
-					
+					switch (choice) {
+
 					case '1':
-						String text = reader.getInputText();
-						if(text.equals("")) {
+						text = reader.getInputText();
+						if (text.equals(""))
 							break;
-						}
-						if(!InputValidator.isValidInput(text)) {
-							System.out.println("Invalid input: "+text);
-							break;
-						}
-						if(text.length() < 3) {
+
+						if (text.length() < 3) {
 							throw new TooFewArgumentsException();
-							
 						}
-						Calculator calc = new Calculator();
-						System.out.println("Result " +calc.calculate(text));
+						break;
+					case '2':
+						text = reader.getInputText();
+						if (text.equals("")) {
+							break;
+						}
+
+						if (text.length() < 3) {
+							throw new TooFewArgumentsException();
+
+						}
 
 						break;
-						
-					case '2':
+					case '3':
+						text = reader.getInputText();
+						if (text.equals("")) {
+							break;
+						}
+
+						if (text.length() < 3) {
+							throw new TooFewArgumentsException();
+
+						}
+
+						break;
+					case '4':
+						text = reader.getInputText();
+						if (text.equals("")) {
+							break;
+						}
+
+						if (text.length() < 3) {
+							throw new TooFewArgumentsException();
+
+						}
+						break;
+					case '5':
 						wantToExit = true;
 						break;
-						
+
 					default:
 						OutputWriter.inValidOption();
 						break;
 					}
 				} catch (TooFewArgumentsException e) {
 					System.out.println("Too few arguments in input, need a second number.");
-				} catch (MixingDifferentCalculationOperatorsException e) {
-					System.out.println("You can't mix different operators.");
-				} catch (HasIllegalInput e) {
-					System.out.println("You inputted illegal characters, legal chars: 0123456789+-/*.");
-				} catch (UsingOperatorAfterAnotherOperator e) {
-					System.out.println("You can't use operators without a number in between, unless its a minus.");
-				} catch (ExceededNumberOfDivisionsLimitException e) {
-					System.out.println("Can only divide two numbers. E.g. 5/2");
-				} catch (ExceededNumberOfMultiplicationsLimitException e) {
-					System.out.println("Can only multiply two numbers. E.g. 5*2");
-				} catch (CanNotDivideByZeroException e) {
-					System.out.println("Can not divide by zero.");
 				} catch (NumberFormatException e) {
 					System.out.println("NumberFormatException was thrown when parsing.");
-				} catch (EndsWithOperatorException e) {
-					System.out.println("Input cant end with operator. - + / *");
-				} catch (StartsWithOperatorException e) {
-					System.out.println("Input cant start with operator. + / *");
 				}
-				
-			
 			}
 		} catch (IOException exception) {
 			OutputWriter.printInputReaderException(exception);
 			OutputWriter.printExit();
 		}
-		
+
 	}
 
 }
--- java1/src/java1/OutputWriter.java
+++ java1/src/java1/OutputWriter.java
@@ -1,40 +1,41 @@
 package java1;
 
-public class OutputWriter{
-	
+public class OutputWriter {
+
 	public static void printMainMenu() {
-		System.out.println("----Main Menu----	");
-		System.out.println("1. Make calculation");
-		System.out.println("2. Exit			");
+		System.out.println("----Main Menu----");
+		System.out.println("1. Addition	");
+		System.out.println("2. Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		System.out.println("5. Exit");
 	}
 
 	public static void inValidOption() {
 		System.out.println("Invalid menu option.");
 	}
-	
+
 	public static void printExit() {
 		System.out.println("The program will now exit.");
 	}
-	
+
 	public static void printChoiceException(Exception exception) {
 		String formatStringmessage = "\"An: %s was thrown when reading menu choice with the message %s \n";
 		printExceptionHelper(exception, formatStringmessage);
 	}
-	
+
 	public static void printTextInputException(Exception exception) {
 		String formatStringmessage = "An: %s was thrown when reading calculation input with the message %s \n";
 		printExceptionHelper(exception, formatStringmessage);
 	}
-	
+
 	public static void printInputReaderException(Exception exception) {
 		String formatStringmessage = "An: %s was thrown with the message %s \n";
 		printExceptionHelper(exception, formatStringmessage);
 	}
-	
+
 	private static void printExceptionHelper(Exception exception, String formatString) {
-		System.err.format(formatString,exception.toString(), exception.getMessage());
+		System.err.format(formatString, exception.toString(), exception.getMessage());
 	}
-	
-	
-	
+
 }
