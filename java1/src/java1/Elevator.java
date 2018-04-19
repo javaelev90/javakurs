f5e16c2cd0b79f6e7cef218f934fd810752c616c
javaelev90 <37899614+javaelev90@users.noreply.github.com>
2018-04-19 14:22:57 +0200

    been working on elevator. saving progress

--- java1/src/java1/Elevator.java
+++ java1/src/java1/Elevator.java
@@ -1,5 +1,60 @@
 package java1;
 
-public class Elevator {
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class Elevator implements Runnable{
+
+	
+	private int numberOfLevels;
+	private int currentLevel;
+	private boolean elevatorOn;
+	private List<Person> work;
+	private List<Person> peopleInElevator;
+	
+	public Elevator(int numberOfLevels, int startingLevel) {
+		
+		if(numberOfLevels < 1) {
+			throw new IllegalArgumentException("Number of levels can't be less than 1.");
+		}
+		if(startingLevel < 0) {
+			throw new IllegalArgumentException("Starting level can't be less than 0.");
+		}
+		if(startingLevel > numberOfLevels) {
+			throw new IllegalArgumentException("Starting level can't be higher than number of levels.");
+		}
+		this.numberOfLevels = numberOfLevels;
+		currentLevel = startingLevel;
+	}
+
+	@Override
+	public void run() {
+		elevatorOn = true;
+//		ExecutorService service = Executors.;
+//		FutureTask<String> task = new FutureTask<String>();
+		while(elevatorOn) {
+			
+			
+			
+		}
+	}
+	
+	public void turnOffElevator() {
+		elevatorOn = false;
+	}
+	
+	
+	
+	public synchronized boolean addToWorkList(Person person) {
+		return work.add(person);
+	}
+	
 
 }
--- java1/src/java1/Program.java
+++ java1/src/java1/Program.java
@@ -3,6 +3,9 @@ package java1;
 public class Program {
 
 	public static void main(String[] args) {
+
+		
+		
 		
 	}
 
